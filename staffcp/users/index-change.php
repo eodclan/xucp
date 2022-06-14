0514a1cd43ad7117767318cf5f70251399ebb4b5
Str1k3r <eodstr1k3r@t-online.de>
2022-06-14 10:39:13 +0200

    Update index-change.php

--- staffcp/users/index-change.php
+++ staffcp/users/index-change.php
@@ -51,7 +51,7 @@ if ($result->num_rows > 0) {
 			<div class='position-fixed bottom-0 end-0 p-3' style='z-index: 11'>
 				<div id='liveToast' class='toast hide' role='alert' aria-live='assertive' aria-atomic='true'>
 					<div class='toast-header'>
-						<strong class='me-auto'>".FRAGE_HEADER."</strong>
+						<strong class='me-auto'>".STAFF_USERCAHNEGED."</strong>
 						<button type='button' class='btn-close' data-bs-dismiss='toast' aria-label='Close'></button>
 					</div>
 					<div class='toast-body'>
