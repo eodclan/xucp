c8e0ce39d03df5fb218974d2ed9787ccdf57c509
Str1k3r <eodstr1k3r@t-online.de>
2022-06-16 15:11:14 +0200

    Update index.php
    code cleaned

--- staffcp/index.php
+++ staffcp/index.php
@@ -5,7 +5,7 @@
 if ( $_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) {        
 	header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
 	setCookie("PHPSESSID", "", 0x7fffffff,  "/");
-  	session_destroy();
-        die( header( 'location: /404.php' ) );
+	session_destroy();
+	die( header( 'location: /404.php' ) );
 }
 ?>
